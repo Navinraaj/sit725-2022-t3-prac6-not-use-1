05f252ec606c28178afb19c1bf99b03f069647da
Navinraaj <117885210+Navinraaj@users.noreply.github.com>
2023-01-04 05:23:10 +1100

    week 6

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(cors())
 app.use('/api/project',projectRoutes)
 //app.use('/api/user',userRoutes)
 
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+  })
 /*
 //mongodb Connection
 const MongoClient = require('mongodb').MongoClient;
@@ -70,7 +79,7 @@ app.get('/api/projects',(req,res) => {
         })
         
      */   
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
 app.listen(port,()=>{
         console.log("App listening to: http://localhost:"+port)
         //createColllection('Homeless People')
